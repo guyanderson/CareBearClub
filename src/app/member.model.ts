6f4406c9e9bd3252b7705d5b0e4d633199565119
guy <luckythelich@hotmail.com>
2017-07-14 10:09:22 -0700

    add member constructor and 2 starting members.

---
+++ src/app/member.model.ts
@@ -0,0 +1,3 @@
+export class Member {
+   constructor (public name: string, public symbol: string, public favorite, public owned: number, public hugs: number, public stare: number, public id: number) { }
+}
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-members',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./members.component.css']
 })
 export class MembersComponent {
+   members: Member[] = [
+      new Member("Jun", "pua aloalo", "Daydream Bear", 23, 9, 6, 1),
+      new Member("David", "?", "Good Luck Bear", 2, 2, 1, 2)
+   ];
 
 }
